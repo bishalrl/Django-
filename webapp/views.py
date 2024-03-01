43e4a1136ef7c31fe412da567024b94fa3f43c0e
bishalrl <aryalbishal9876@gmail.com>
2024-03-01 15:16:31 +0545

    register

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .forms import CreateUserForm, LoginForm
 # Create your views here.
 def home(request):
  return render(request,'webapp/index.html')
+
+
+
+# Register
+def register(request):
+    
+    form = CreateUserForm()
+    if request.method == "Post":
+        form = CreateUserForm(request.post)
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+            
+    context = {'form':form}
+    return render(request,'webapp/register.html',context=context)
